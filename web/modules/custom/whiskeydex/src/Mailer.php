1ac0c90946f00fea6faa8a3ebd3b8946cbed9161
Matt Glaman <nmd.matt@gmail.com>
2022-06-25 11:50:59 -0500

    setup symfony mailer

---
+++ web/modules/custom/whiskeydex/src/Mailer.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace Drupal\whiskeydex;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Mail\MailManagerInterface;
+
+final class Mailer {
+
+  public function __construct(
+    private readonly MailManagerInterface $mailManager,
+    private readonly LanguageManagerInterface $languageManager,
+    private readonly ConfigFactoryInterface $configFactory
+  ) {
+  }
+
+  public function sendMail(string $to, string $subject, array $body, array $params = []): bool {
+    $default_params = [
+      'headers' => [
+        'Content-Type' => 'text/html; charset=UTF-8;',
+        'Content-Transfer-Encoding' => '8Bit',
+      ],
+      'id' => 'mail',
+      'from' => $this->configFactory->get('system.site')->get('mail_notification'),
+      'reply-to' => NULL,
+      'subject' => $subject,
+      'langcode' => $this->languageManager->getCurrentLanguage()->getId(),
+      'body' => $body,
+    ];
+    if (!empty($params['cc'])) {
+      $default_params['headers']['Cc'] = $params['cc'];
+    }
+    if (!empty($params['bcc'])) {
+      $default_params['headers']['Bcc'] = $params['bcc'];
+    }
+    $params = array_replace($default_params, $params);
+    $message = $this->mailManager->mail('whiskeydex', $params['id'], $to, $params['langcode'], $params, $params['reply-to']);
+    $message += ['to' => $to];
+    return (bool) $message['result'];
+  }
+}
